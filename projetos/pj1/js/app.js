9b7b5c6d3ec41bde77beb43592d14dba24dd198d
brunoseventeen <ls.bruno.ls.bruno@gmail.com>
2024-09-06 10:39:33 -0300

    subindo

---
+++ projetos/pj1/js/app.js
@@ -0,0 +1,18 @@
+const carousel = document.querySelector('#carouselExampleManual');
+const slides = carousel.querySelectorAll('.carousel-item');
+let currentSlide = 0;
+
+function nextSlide() {
+  slides[currentSlide].classList.remove('active');
+  currentSlide = (currentSlide + 1) % slides.length;
+  slides[currentSlide].classList.add('active');   
+
+}
+
+function prevSlide() {
+  slides[currentSlide].classList.remove('active');
+  currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+  slides[currentSlide].classList.add('active');   
+  
+
+}
